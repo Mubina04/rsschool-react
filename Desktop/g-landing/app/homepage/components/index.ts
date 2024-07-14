3ff2f9cdedc5eba3b6e32e8e100960b82de06634
Mubina04 <110577535+Mubina04@users.noreply.github.com>
2024-07-14 12:46:48 +0500

    Delete Desktop/g-landing directory

--- Desktop/g-landing/app/homepage/components/index.ts
+++
@@ -1,6 +0,0 @@
-export {default as Header} from './Header';
-export {default as Groups} from './Groups';
-export {default as AboutUs} from './AboutUs';
-export {default as Services} from './Services';
-export {default as Contacts} from './Contacts';
-export {default as News} from './News';
--- Desktop/g-landing/styles/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as homeStyles} from './home/Home.module.css'
-export {default as navbarStyles} from './navbar/Navbar.module.css'
-export {default as footerStyles} from './footer/Footer.module.css'
-export {default as cardStyles} from './card/Card.module.css'
